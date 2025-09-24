428114efcf635e5c1976d11679bd305815318164
Vlad Galan <stefan-vladimir.galan@student.nhh.no>
2025-09-24 09:20:20 +0200

    "please work"

---
+++ lectures/lecture1/lecture1_sg.py
@@ -0,0 +1,31 @@
+"""
+Part 2, Lecture 1
+
+Implement and test argmax() function that returns the location of a maximum.
+
+Tasks
+-----
+
+1.  Implement a function argmax() that takes a sequence of numbers and returns 
+    the index (position) the maximum element.
+
+2.  Test the function with the following sequence of numbers:
+    [2, 3, -1, 7, 4]
+
+3.  Add error handling if an empty sequence is passed. Test the function with an 
+    empty sequence.
+
+4.  Use the notebook lecture1-benchmark.ipynb to benchmark your implementation 
+    against NumPy's argmax().
+"""
+
+import numpy as np
+
+def argmax(Listin:list) -> int:
+    index_dict = {Listin[i]:i for i in range(0, len(Listin))} 
+    max_index = index_dict[np.array(Listin).max()]
+    print(index_dict)
+    return max_index
+
+print(argmax([1, 10, -1, 7, 4]))
+
